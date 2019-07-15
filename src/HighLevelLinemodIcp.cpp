fd4868715044cb991dc46d4a82c87e049615f8d7
Anton Paul Seymour Elmiger <elmiger@campus.tu-berlin.de>
2019-07-15 14:51:20 +0200

    fixed wrong principal point handeling

--- src/HighLevelLinemodIcp.cpp
+++ src/HighLevelLinemodIcp.cpp
@@ -106,7 +106,6 @@ uint16_t HighLevelLinemodIcp::estimateBestMatch(cv::Mat in_depthImg, std::vector
 		absdiff(maskedDepthImg, maskedDepth, diffImg);
 
 		cv::Scalar mean = cv::mean(diffImg, binary);
-		std::cout << "mean: " << mean << " it: " << i << std::endl;
 
 		if ((mean[0] < bestMean && mean[0] != 0) || i == 0)
 		{
