d87052a876a3d1cb0cb418850277325139ed93de
Anton Paul Seymour Elmiger <elmiger@campus.tu-berlin.de>
2019-08-20 18:04:09 +0200

    move decleration to source file

--- include/ModelBuffer.h
+++ include/ModelBuffer.h
@@ -6,39 +6,11 @@
 
 struct ModelBuffer
 {
-	ModelBuffer(void* in_vertData, uint32_t in_numVertices, void* in_indData, uint32_t in_numIndices, uint8_t in_elementSize)
-	{
-		glGenBuffers(1, &indBufferId);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indBufferId);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, in_numIndices * in_elementSize, in_indData, GL_STATIC_DRAW);
-		numIndices = in_numIndices;
+	ModelBuffer(void* in_vertData, uint32_t in_numVertices, void* in_indData, uint32_t in_numIndices, uint8_t in_elementSize);
 
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
+	void bind();
 
-		glGenBuffers(1, &vertBufferId);
-		glBindBuffer(GL_ARRAY_BUFFER, vertBufferId);
-		glBufferData(GL_ARRAY_BUFFER, in_numVertices * 2 * sizeof(glm::vec3), in_vertData, GL_STATIC_DRAW);
-
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3) * 2, (void*)offsetof(glm::vec3, x));
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3) * 2, (void*)12);
-
-		glBindVertexArray(0);
-	}
-
-	void bind()
-	{
-		glBindVertexArray(vao);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indBufferId);
-	}
-
-	void unbind()
-	{
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
-	}
+	void unbind();
 
 public:
 	GLuint numIndices;
